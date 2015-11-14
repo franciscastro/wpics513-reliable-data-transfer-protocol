33c0a87d0cf1a7d5501ccb4c981b8162849c8c1f
Francis Castro <fgcastro@wpi.edu>
2015-11-14 13:45:03 -0500

    Correct error

--- config.h
+++ config.h
@@ -4,3 +4,20 @@
 #define MAXDATASIZE 1000 // max number of bytes we can get at once
 #define DROPRATE 50
 #define CORRUPTRATE 50
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
