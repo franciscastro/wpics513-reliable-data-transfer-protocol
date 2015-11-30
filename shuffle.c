59203378cd83baa39c105716d2d4b7b6c5b3ae4d
Francis Castro <fgcastro@wpi.edu>
2015-11-30 14:23:59 -0500

    Added shuffle.c

---
+++ shuffle.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+ 
+int rrand(int m) {
+  return (int)((double)m * ( rand() / (RAND_MAX+1.0) ));
+}
+ 
+#define BYTE(X) ((unsigned char *)(X))
+
+void shuffle(void *obj, size_t nmemb, size_t size) {
+  void *temp = malloc(size);
+  size_t n = nmemb;
+  while ( n > 1 ) {
+    size_t k = rrand(n--);
+    memcpy(temp, BYTE(obj) + n*size, size);
+    memcpy(BYTE(obj) + n*size, BYTE(obj) + k*size, size);
+    memcpy(BYTE(obj) + k*size, temp, size);
+  }
+  printf("%s\n", temp);
+  free(temp);
+}
+
+int main(int argc, char const *argv[]) {
+	
+	shuffle(argv[1], strlen(argv[1]), sizeof(argv[1]));
+
+	return 0;
+}
